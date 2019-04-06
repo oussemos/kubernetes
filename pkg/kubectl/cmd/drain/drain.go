c84b6c818bb33b994e9916a8452b858451268716
Oussema CHERNI <oussemos@gmail.com>
2019-04-06 20:20:50 +0200

    Fix kubectl drain docs

--- pkg/kubectl/cmd/drain/drain.go
+++ pkg/kubectl/cmd/drain/drain.go
@@ -116,17 +116,16 @@ var (
 		Drain node in preparation for maintenance.
 
 		The given node will be marked unschedulable to prevent new pods from arriving.
-		'drain' evicts the pods if the APIServer supports
-		[eviction](http://kubernetes.io/docs/admin/disruptions/). Otherwise, it will use normal
-		DELETE to delete the pods.
+		'drain' evicts the pods if the APIServer supports [eviction](https://kubernetes.io/docs/concepts/workloads/pods/disruptions/).
+		Otherwise, it will use normal DELETE to delete the pods.
 		The 'drain' evicts or deletes all pods except mirror pods (which cannot be deleted through
-		the API server).  If there are DaemonSet-managed pods, drain will not proceed
+		the API server). If there are DaemonSet-managed pods, drain will not proceed
 		without --ignore-daemonsets, and regardless it will not delete any
 		DaemonSet-managed pods, because those pods would be immediately replaced by the
-		DaemonSet controller, which ignores unschedulable markings.  If there are any
+		DaemonSet controller, which ignores unschedulable markings. If there are any
 		pods that are neither mirror pods nor managed by ReplicationController,
 		ReplicaSet, DaemonSet, StatefulSet or Job, then drain will not delete any pods unless you
-		use --force.  --force will also allow deletion to proceed if the managing resource of one
+		use --force. --force will also allow deletion to proceed if the managing resource of one
 		or more pods is missing.
 
 		'drain' waits for graceful termination. You should not operate on the machine until
@@ -135,7 +134,7 @@ var (
 		When you are ready to put the node back into service, use kubectl uncordon, which
 		will make the node schedulable again.
 
-		![Workflow](http://kubernetes.io/images/docs/kubectl_drain.svg)`))
+		![Workflow](https://kubernetes.io/images/docs/kubectl_drain.svg)`))
 
 	drainExample = templates.Examples(i18n.T(`
 		# Drain node "foo", even if there are pods not managed by a ReplicationController, ReplicaSet, Job, DaemonSet or StatefulSet on it.
